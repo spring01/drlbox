b895b5c31d1a1b5c9bc8275c9fedca4e17ecf0d3
spring01 <lihc2012@gmail.com>
2017-05-22 22:29:19 -0400

    observation_space shape fix

--- wrapper/ppss.py
+++ wrapper/ppss.py
@@ -7,16 +7,19 @@ class PPSSWrapper(gym.Wrapper):
     """
         A wrapper for frame PreProcessing, frame Stacking, and frame Skipping.
         Will convert input image to grayscale, resize to `resize`,
-        stack `stack_frames` most recent resized frames together,
+        stack `num_frames` most recent resized frames together,
         and perform action for `act_steps` steps.
     """
 
     metadata = {'render.modes': ['human', 'grayscale_array']}
 
-    def __init__(self, env, resize=(84, 110), stack_frames=4, act_steps=2):
+    def __init__(self, env, resize=(84, 110), num_frames=4, act_steps=2):
         super(PPSSWrapper, self).__init__(env)
+        width, height = resize
+        shape = height, width, num_frames
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=shape)
         self.resize = resize
-        self.stack_frames = stack_frames
+        self.num_frames = num_frames
         self.act_steps = act_steps
         self.stepcount = 0
         self.viewer = None
@@ -33,16 +36,19 @@ class PPSSWrapper(gym.Wrapper):
             total_reward += reward
             current_step += 1
         if 'ppss.stepcount' in info:
-            raise gym.error.Error('Key "ppss.stepcount" already in info. Make sure you are not stacking ' \
+            raise gym.error.Error('Key "ppss.stepcount" already in info. '
+                                  'Make sure you are not stacking '
                                   'the PPSSWrapper wrappers.')
         info['ppss.stepcount'] = self.stepcount
-        return self.state, total_reward, done, info
+        self.stack_state = np.stack(self.state, axis=2)
+        return self.stack_state.astype(np.float32), total_reward, done, info
 
     def _reset(self):
         self.stepcount = 0
         obs = self.env.reset()
-        self.state = [self.preprocess(obs)] * self.stack_frames
-        return self.state
+        self.state = [self.preprocess(obs)] * self.num_frames
+        self.stack_state = np.stack(self.state, axis=2)
+        return self.stack_state.astype(np.float32)
 
     def _render(self, mode='human', close=False):
         if close:
@@ -50,14 +56,13 @@ class PPSSWrapper(gym.Wrapper):
                 self.viewer.close()
                 self.viewer = None
             return
-        img = np.stack(self.state, axis=2)
         if mode == 'grayscale_array':
-            return img
+            return self.stack_state
         elif mode == 'human':
             from gym.envs.classic_control import rendering
             if self.viewer is None:
                 self.viewer = rendering.SimpleImageViewer()
-            img = np.stack([np.max(img, axis=2)] * 3, axis=2)
+            img = np.stack([np.max(self.stack_state, axis=2)] * 3, axis=2)
             self.viewer.imshow(img)
 
     def preprocess(self, obs):
