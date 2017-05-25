1fb1fe16c94f11bd5b9df39d12402dcd6c29a55d
spring01 <lihc2012@gmail.com>
2017-05-24 22:48:38 -0400

    dqn util.py

---
+++ dqn/util.py
@@ -0,0 +1,23 @@
+
+import os
+import subprocess
+
+
+def get_output_folder(parent_dir, env_name):
+    experiment_id = 0
+    for folder_name in os.listdir(parent_dir):
+        if not os.path.isdir(os.path.join(parent_dir, folder_name)):
+            continue
+        try:
+            folder_name = int(folder_name.split('-run')[-1])
+            if folder_name > experiment_id:
+                experiment_id = folder_name
+        except:
+            pass
+    experiment_id += 1
+
+    parent_dir = os.path.join(parent_dir, env_name)
+    parent_dir += '-run{}'.format(experiment_id)
+    subprocess.call(["mkdir", "-p", parent_dir])
+    return parent_dir
+
