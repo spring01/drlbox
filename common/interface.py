0752d3b10f873f55d9ffefc69e048585aff0d9b6
spring01 <lihc2012@gmail.com>
2017-07-20 15:01:29 -0400

    add list_arrays_ravel in common.interface

--- common/interface.py
+++ common/interface.py
@@ -9,3 +9,10 @@ to a correctly shaped numpy array which can be fed into the neural network
 def list_frames_to_array(state):
     return np.stack(state, axis=-1).astype(np.float32)
 
+'''
+When a state is represented as a list of arrays, this interface converts it
+to a correctly ravelled numpy array which can be fed into the neural network
+'''
+def list_arrays_ravel(state):
+    return np.stack(state, axis=-1).ravel()
+
