9e68453d09f875450833db438b4c264c8fb91395
spring01 <lihc2012@gmail.com>
2017-10-17 17:03:40 -0400

    comment change in reward clipper

--- common/envwrapper/reward.py
+++ common/envwrapper/reward.py
@@ -5,15 +5,13 @@ import gym
 class RewardClipper(gym.Wrapper):
     """
         A wrapper for reward clipping.
-        Stacks `num_frames` most recent frames together,
-        and performs action for `act_steps` steps each time env.step is called.
     """
 
     '''
         Arguments for the constructor:
         env: Game environment to be history stacking wrapped;
-        num_frames: Number of frames to be stacked together; integer;
-        act_steps: Number of actions performed between states; integer.
+        lower: lower bound of reward; float;
+        upper: upper bound of reward; float.
     '''
     def __init__(self, env, lower=-1.0, upper=1.0):
         super(RewardClipper, self).__init__(env)
