521433353ff41b609d7bb281749b211304d451e0
spring01 <lihc2012@gmail.com>
2017-10-18 14:32:00 -0400

    clean_exit in testing scripts

--- a3c_test.bash
+++ a3c_test.bash
@@ -1,37 +1,41 @@
 #!/bin/bash
 
+function clean_exit() {
+    echo "rm -rf $output"
+    rm -rf $output
+    exit $1
+}
 
 output="$(mktemp -d)"
 echo "made temp dir $output"
 
 # cartpole a3c fc
-python a3c_trainer.py \
-    --rl_train_steps 500 --interval_save 200 --rl_save_path $output || exit 1
+python a3c_trainer.py --rl_train_steps 500 --rl_save_path $output \
+    --interval_save 200 || clean_exit 1
 
 # cartpole evaluator
 cartpole_weights="$(ls $output/CartPole-v0-run1/weights_*.p | tail -n 1)"
 python evaluator.py --net_type acnet --read_weights $cartpole_weights \
-    --policy_type stochastic --render false --eval_episodes 3 || exit 1
+    --policy_type stochastic --render false --eval_episodes 3 || clean_exit 1
 
 # breakout a3c fc
 python a3c_trainer.py --dtf_num_workers 8 \
     --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --interval_save 200 --rl_save_path $output \
-    --additional model.atari_nets fc 256 || exit 1
+    --additional model.atari_nets fc 256 || clean_exit 1
 
 # breakout a3c lstm
 python a3c_trainer.py --dtf_num_workers 8 \
     --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
-    --rl_train_steps 500 --interval_save 200 --rl_save_path $output \
-    --additional model.atari_nets lstm 64 || exit 1
+    --rl_train_steps 500 --rl_save_path $output --interval_save 200 \
+    --additional model.atari_nets lstm 64 || clean_exit 1
 
 # breakout evaluator
 breakout_weights="$(ls $output/Breakout-v0-run3/weights_*.p | tail -n 1)"
 python evaluator.py --net_type acnet --read_weights $breakout_weights \
     --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --policy_type stochastic --render false --eval_episodes 3 \
-    --additional model.atari_nets lstm 64 --net_type acnet || exit 1
+    --additional model.atari_nets lstm 64 --net_type acnet || clean_exit 1
 
-echo "rm -rf $output"
-rm -rf $output
+clean_exit 0
 
--- dqn_test.bash
+++ dqn_test.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function clean_exit() {
+    echo "rm -rf $output"
+    rm -rf $output
+    exit $1
+}
 
 output="$(mktemp -d)"
 echo "made temp dir $output"
@@ -7,48 +12,47 @@ echo "made temp dir $output"
 # cartpole dqn
 python dqn_trainer.py --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
-    --memory_maxlen 200 --memory_fill 100 || exit 1
+    --memory_maxlen 200 --memory_fill 100 || clean_exit 1
 
 # cartpole evaluator
 cartpole_weights="$(ls $output/CartPole-v0-run1/weights_*.p | tail -n 1)"
 python evaluator.py --net_type qnet --read_weights $cartpole_weights \
-    --policy_eps 0.05 --render false --eval_episodes 3 || exit 1
+    --policy_eps 0.05 --render false --eval_episodes 3 || clean_exit 1
 
 # breakout dqn fc
 python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
-    --additional model.atari_nets 'fc' 256 || exit 1
+    --additional model.atari_nets 'fc' 256 || clean_exit 1
 
 # breakout dqn dueling fc
 python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
-    --additional model.atari_nets 'dueling fc' 256 || exit 1
+    --additional model.atari_nets 'dueling fc' 256 || clean_exit 1
 
 # breakout dqn lstm
 python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
-    --additional model.atari_nets 'lstm' 64 || exit 1
+    --additional model.atari_nets 'lstm' 64 || clean_exit 1
 
 # breakout dqn dueling gru
 python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
-    --additional model.atari_nets 'dueling gru' 64 || exit 1
+    --additional model.atari_nets 'dueling gru' 64 || clean_exit 1
 
 # breakout evaluator
 breakout_weights="$(ls $output/Breakout-v0-run3/weights_*.p | tail -n 1)"
 python evaluator.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --net_type qnet --read_weights $breakout_weights \
     --policy_eps 0.05 --render false --eval_episodes 3 \
-    --additional model.atari_nets 'dueling fc' 256 || exit 1
+    --additional model.atari_nets 'dueling fc' 256 || clean_exit 1
 
-echo "rm -rf $output"
-rm -rf $output
+clean_exit 0
 
