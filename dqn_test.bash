8fd24d1618a1dd54cbab0dcf2ead28d981214d51
spring01 <lihc2012@gmail.com>
2017-10-18 00:06:52 -0400

    use unified trainer/evaluator in test scripts

--- dqn_test.bash
+++ dqn_test.bash
@@ -4,35 +4,50 @@
 output="$(mktemp -d)"
 echo "made temp dir $output"
 
-# fully connected
-python dqn_atari_trainer.py --rl_train_steps 500 \
+# cartpole dqn
+python dqn_trainer.py --rl_train_steps 500 --rl_save_path $output \
+    --interval_sync_target 200 --interval_save 200 \
+    --memory_maxlen 200 --memory_fill 100 || exit 1
+
+# cartpole evaluator
+cartpole_weights="$(ls $output/CartPole-v0-run1/weights_*.p | tail -n 1)"
+python evaluator.py --net_type qnet --read_weights $cartpole_weights \
+    --policy_eps 0.05 --render false --eval_episodes 3 || exit 1
+
+# breakout dqn fc
+python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+    --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
-    --net_name 'fc' --net_size 256 --rl_save_path $output
+    --additional model.atari_nets 'fc' 256 || exit 1
 
-# dueling fully connected
-python dqn_atari_trainer.py --rl_train_steps 500 \
+# breakout dqn dueling fc
+python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+    --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
-    --net_name 'dueling fc' --net_size 256 --rl_save_path $output
+    --additional model.atari_nets 'dueling fc' 256 || exit 1
 
-# lstm
-python dqn_atari_trainer.py --rl_train_steps 500 \
+# breakout dqn lstm
+python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+    --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
-    --net_name 'lstm' --net_size 128 --rl_save_path $output
+    --additional model.atari_nets 'lstm' 64 || exit 1
 
-# dueling gru
-python dqn_atari_trainer.py --rl_train_steps 500 \
+# breakout dqn dueling gru
+python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+    --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
-    --net_name 'dueling gru' --net_size 128 --rl_save_path $output
+    --additional model.atari_nets 'dueling gru' 64 || exit 1
 
-# evaluator
-python atari_evaluator.py --read_weights $output/Breakout-v0-run2/weights_4*.p \
-    --policy_eps 0.05 --render False \
-    --net_type 'qnet' --net_name 'dueling fc' --net_size 256 \
-    --eval_episodes 3
+# breakout evaluator
+breakout_weights="$(ls $output/Breakout-v0-run3/weights_*.p | tail -n 1)"
+python evaluator.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+    --net_type qnet --read_weights $breakout_weights \
+    --policy_eps 0.05 --render false --eval_episodes 3 \
+    --additional model.atari_nets 'dueling fc' 256 || exit 1
 
 echo "rm -rf $output"
 rm -rf $output
