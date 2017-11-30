e30d93baed899945df5d77282c3a2c8cf6333a10
spring01 <lihc2012@gmail.com>
2017-11-30 15:48:41 -0500

    change testing bash scripts to call the new executable scripts

--- a3c_test.bash
+++ a3c_test.bash
@@ -10,35 +10,35 @@ output="$(mktemp -d)"
 echo "made temp dir $output"
 
 # cartpole a3c fc
-python a3c_trainer.py --rl_train_steps 500 --rl_save_path $output \
+hcdrl_a3c.py --rl_train_steps 500 --rl_save_path $output \
     --interval_save 200 || clean_exit 1
 
 # cartpole evaluator
 cartpole_weights="$(ls $output/CartPole-v0-run1/weights_*.p | tail -n 1)"
-python evaluator.py --net_type acnet --read_weights $cartpole_weights \
+hcdrl_evaluator.py --net_type acnet --read_weights $cartpole_weights \
     --policy_type stochastic --render false --eval_episodes 3 || clean_exit 1
 
 # breakout a3c fc
-python a3c_trainer.py --dtf_num_workers 8 \
+hcdrl_a3c.py --dtf_num_workers 8 \
     --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --interval_save 200 --rl_save_path $output \
     --additional hcdrl.model.atari_nets fc 256 || clean_exit 1
 
 # breakout a3c lstm
-python a3c_trainer.py --dtf_num_workers 8 \
+hcdrl_a3c.py --dtf_num_workers 8 \
     --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output --interval_save 200 \
     --additional hcdrl.model.atari_nets lstm 64 || clean_exit 1
 
 # breakout evaluator
 breakout_weights="$(ls $output/Breakout-v0-run3/weights_*.p | tail -n 1)"
-python evaluator.py --net_type acnet --read_weights $breakout_weights \
+hcdrl_evaluator.py --net_type acnet --read_weights $breakout_weights \
     --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --policy_type stochastic --render false --eval_episodes 3 \
     --additional hcdrl.model.atari_nets lstm 64 --net_type acnet || clean_exit 1
 
 # breakout a3c lstm load weights
-python a3c_trainer.py --dtf_num_workers 8 --rl_load_weights $breakout_weights \
+hcdrl_a3c.py --dtf_num_workers 8 --rl_load_weights $breakout_weights \
     --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output --interval_save 200 \
     --additional hcdrl.model.atari_nets lstm 64 || clean_exit 1
--- dqn_test.bash
+++ dqn_test.bash
@@ -10,38 +10,38 @@ output="$(mktemp -d)"
 echo "made temp dir $output"
 
 # cartpole dqn
-python dqn_trainer.py --rl_train_steps 500 --rl_save_path $output \
+hcdrl_dqn.py --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 || clean_exit 1
 
 # cartpole evaluator
 cartpole_weights="$(ls $output/CartPole-v0-run1/weights_*.p | tail -n 1)"
-python evaluator.py --net_type qnet --read_weights $cartpole_weights \
+hcdrl_evaluator.py --net_type qnet --read_weights $cartpole_weights \
     --policy_eps 0.05 --render false --eval_episodes 3 || clean_exit 1
 
 # breakout dqn fc
-python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+hcdrl_dqn.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
     --additional hcdrl.model.atari_nets 'fc' 256 || clean_exit 1
 
 # breakout dqn dueling fc
-python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+hcdrl_dqn.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
     --additional hcdrl.model.atari_nets 'dueling fc' 256 || clean_exit 1
 
 # breakout dqn lstm
-python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+hcdrl_dqn.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
     --additional hcdrl.model.atari_nets 'lstm' 64 || clean_exit 1
 
 # breakout dqn dueling gru
-python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+hcdrl_dqn.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --rl_train_steps 500 --rl_save_path $output \
     --interval_sync_target 200 --interval_save 200 \
     --memory_maxlen 200 --memory_fill 100 \
@@ -49,7 +49,7 @@ python dqn_trainer.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
 
 # breakout evaluator
 breakout_weights="$(ls $output/Breakout-v0-run3/weights_*.p | tail -n 1)"
-python evaluator.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
+hcdrl_evaluator.py --env Breakout-v0 --env_num_frames 4 --env_act_steps 4 \
     --net_type qnet --read_weights $breakout_weights \
     --policy_eps 0.05 --render false --eval_episodes 3 \
     --additional hcdrl.model.atari_nets 'dueling fc' 256 || clean_exit 1
