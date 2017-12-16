correct continuous acnet using a spherical gaussian covariance matrix

drlbox/a3c/acnet.py
@@ -27,13 +27,13 @@ class ACNet(RLNet):
             log_probs_act = tf.reduce_sum(log_probs * action_1h, axis=1)
             neg_entropy = tf.reduce_sum(probs * log_probs) * entropy_weight
         elif self.action_mode == CONTINUOUS:
-            dim_action = tf_logits.shape[1] // 2
+            dim_action = tf_logits.shape[1] - 1
             ph_action = tf.placeholder(tf.int32, [None, dim_action])
-            mean = tf_logits[:, :dim_action]
-            var = tf_logits[:, dim_action:]
-            var = tf.log(tf.exp(var) + 1.0)
+            mean = tf_logits[:, :-1]
+            var = tf.nn.softplus(tf_logits[:, -1:])
             log_norm = tf.reduce_sum((ph_act - mean)**2 / (2.0 * var), axis=1)
-            factor = 0.5 * tf.reduce_sum(np.log(2 * np.pi) + tf.log(var), axis=1)
+            factor = tf.reduce_sum(np.log(2 * np.pi) + tf.log(var), axis=1)
+            factor *= 0.5 * dim_action
             log_probs_act = -(log_norm + factor)
             neg_entropy = tf.reduce_sum(tf.log(2.0 * mean * var) + 1.0)
             neg_entropy *= entropy_weight * 0.5