700b3452aca4c84a168e5ac636cc71d8f33ae743
spring01 <lihc2012@gmail.com>
2018-01-06 23:38:23 -0500

    fully connected totally separated value/logits streams

---
+++ drlbox/feature/fc_sep.py
@@ -0,0 +1,26 @@
+
+from tensorflow.python.keras import layers
+from .preact_layers import DensePreact
+
+
+def state_to_input(state):
+    return state.ravel()
+
+'''
+Input arguments:
+    observation_space: Observation space of the environment;
+    arch_str:          Architecture of the actor-critic net, e.g., '16 16 16'.
+'''
+def feature(observation_space, arch_str):
+    net_arch = arch_str.split(' ')
+    net_arch = [int(num) for num in net_arch]
+    state = layers.Input(shape=observation_space.shape)
+    feature1 = state
+    for num_hid in net_arch:
+        feature1 = DensePreact(num_hid, activation='relu')(feature1)
+    feature2 = state
+    for num_hid in net_arch:
+        feature2 = DensePreact(num_hid, activation='relu')(feature2)
+    return state, (feature1, feature2)
+
+
