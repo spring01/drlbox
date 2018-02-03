2d7df94daf21c76cf51d42df0a2bb10534f7f518
spring01 <lihc2012@gmail.com>
2018-02-03 17:03:59 -0500

    allow specifying unnecessary arguments with warnings

--- drlbox/common/util.py
+++ drlbox/common/util.py
@@ -1,4 +1,5 @@
 
+import sys
 import gym.spaces
 
 
@@ -8,6 +9,7 @@ def discrete_action(action_space):
 def continuous_action(action_space):
     return type(action_space) is gym.spaces.box.Box
 
+WARN_ARG_NOT_USED = 'Warning: argument "{}" set but not used'
 def set_args(obj, default_kwargs, kwargs):
     # set default arguments
     for keyword, value in default_kwargs.items():
@@ -15,6 +17,6 @@ def set_args(obj, default_kwargs, kwargs):
     # replace with user-specified arguments
     for keyword, value in kwargs.items():
         if keyword not in default_kwargs:
-            raise ValueError('Argument "{}" not valid'.format(keyword))
+            print(WARN_ARG_NOT_USED.format(keyword), file=sys.stderr)
         setattr(obj, keyword, value)
 
