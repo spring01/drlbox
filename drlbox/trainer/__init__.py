40dc5a89cbe858df75c587412944dfc8133663f0
spring01 <lihc2012@gmail.com>
2018-02-06 23:25:21 -0500

    add acer support in make_trainer

--- drlbox/trainer/__init__.py
+++ drlbox/trainer/__init__.py
@@ -1,7 +1,2 @@
 
 from .make_trainer import make_trainer
-from .a3c_trainer import A3CTrainer
-from .acktr_trainer import ACKTRTrainer
-from .dqn_trainer import DQNTrainer
-from .noisynet_a3c_trainer import NoisyNetA3CTrainer
-from .noisynet_dqn_trainer import NoisyNetDQNTrainer
--- drlbox/trainer/make_trainer.py
+++ drlbox/trainer/make_trainer.py
@@ -1,6 +1,7 @@
 
 from .a3c_trainer import A3CTrainer
 from .acktr_trainer import ACKTRTrainer
+from .acer_trainer import ACERTrainer
 from .dqn_trainer import DQNTrainer
 from .noisynet_a3c_trainer import NoisyNetA3CTrainer
 from .noisynet_dqn_trainer import NoisyNetDQNTrainer
@@ -8,6 +9,7 @@ from .noisynet_dqn_trainer import NoisyNetDQNTrainer
 
 TRAINER_CLS_DICT = {'a3c':          A3CTrainer,
                     'acktr':        ACKTRTrainer,
+                    'acer':         ACERTrainer,
                     'noisynet-a3c': NoisyNetA3CTrainer,
                     'dqn':          DQNTrainer,
                     'noisynet-dqn': NoisyNetDQNTrainer,}
