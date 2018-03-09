93d0a7bf24e646876819306fb3d9f1f29c5bd740
spring01 <lihc2012@gmail.com>
2018-03-08 19:37:58 -0500

    add an argument controlling regular dqn vs double dqn

--- drlbox/trainer/dqn_trainer.py
+++ drlbox/trainer/dqn_trainer.py
@@ -10,7 +10,8 @@ from .trainer_base import Trainer
 class DQNTrainer(Trainer):
 
     KEYWORD_DICT = {**Trainer.KEYWORD_DICT,
-                    **dict(policy_eps_start=1.0,
+                    **dict(dqn_double=True,
+                           policy_eps_start=1.0,
                            policy_eps_end=0.01,
                            policy_eps_decay_steps=1000000,
                            interval_sync_target=1000,
@@ -51,21 +52,31 @@ class DQNTrainer(Trainer):
     def concat_bootstrap(self, cc_state, rl_slice):
         last_states = [cc_state[r_slice][-1] for r_slice in rl_slice]
         last_states = np.array(last_states)
-        online_last = self.online_net.action_values(last_states)
-        target_last = self.target_net.action_values(last_states)
-        value_shape = cc_state.shape[0], *online_last.shape[1:]
-        cc_online_value = np.zeros(value_shape)
-        cc_target_value = np.zeros(value_shape)
-        for r_slice, o_last, t_last in zip(rl_slice, online_last, target_last):
-            cc_online_value[r_slice][-1] = o_last
-            cc_target_value[r_slice][-1] = t_last
-        return cc_online_value, cc_target_value
+        concat_len = cc_state.shape[0]
+        cc_target_value = self.concat_value(self.target_net, concat_len,
+                                            last_states, rl_slice)
+        if self.dqn_double:
+            cc_online_value = self.concat_value(self.online_net, concat_len,
+                                                last_states, rl_slice)
+            return cc_target_value, cc_online_value
+        else:
+            return cc_target_value,
 
-    def rollout_feed(self, rollout, r_online_value, r_target_value):
+    def concat_value(self, net, concat_len, last_states, rl_slice):
+        last_values = net.action_values(last_states)
+        value_shape = concat_len, *last_values.shape[1:]
+        cc_value = np.zeros(value_shape)
+        for r_slice, last_val in zip(rl_slice, last_values):
+            cc_value[r_slice][-1] = last_val
+        return cc_value
+
+    def rollout_feed(self, rollout, r_target_value, r_online_value=None):
         r_action = np.array(rollout.action_list)
-        online_last_value = r_online_value[-1]
-        target_last_value = r_target_value[-1]
-        target_last_q = target_last_value[online_last_value.argmax()]
+        if self.dqn_double:
+            greedy_last_value = r_online_value[-1]
+        else:
+            greedy_last_value = r_target_value[-1]
+        target_last_q = r_target_value[-1][greedy_last_value.argmax()]
         r_target = self.rollout_target(rollout, target_last_q)
         return r_action, r_target
 
