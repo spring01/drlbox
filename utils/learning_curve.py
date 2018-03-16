2824d68aec5b2522378f3cf12586e53f6b268740
spring01 <lihc2012@gmail.com>
2018-03-16 18:13:32 -0400

    change padding mode when drawing learning curves

--- utils/learning_curve.py
+++ utils/learning_curve.py
@@ -5,11 +5,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def padding(signal, window_size):
+    pad = np.mean(signal[:window_size])
+    return np.concatenate([[pad] * (window_size - 1), signal])
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Draw a learning curve')
-    parser.add_argument('--window_small', default=100, type=int,
+    parser.add_argument('--window-small', default=100, type=int,
         help='Small averaging window for instantaneous performance')
-    parser.add_argument('--window_large', default=1000, type=int,
+    parser.add_argument('--window-large', default=1000, type=int,
         help='Large averaging window for average performance')
 
     args, unknown_args = parser.parse_known_args()
@@ -29,12 +34,14 @@ if __name__ == '__main__':
     num_episode = len(all_rewards)
 
     win_small = np.ones(args.window_small) / args.window_small
-    mean_small = np.convolve(all_rewards, win_small, mode='valid')
+    all_rewards_padded_small = padding(all_rewards, args.window_small)
+    mean_small = np.convolve(all_rewards_padded_small, win_small, mode='valid')
     len_mean_small = len(mean_small)
     space_small = np.linspace(1, num_episode, len_mean_small)
 
     win_large = np.ones(args.window_large) / args.window_large
-    mean_large = np.convolve(all_rewards, win_large, mode='valid')
+    all_rewards_padded_large = padding(all_rewards, args.window_large)
+    mean_large = np.convolve(all_rewards_padded_large, win_large, mode='valid')
     space_large = np.linspace(1, num_episode, len(mean_large))
 
     peak_pos = mean_small.argmax() / len_mean_small * max_step
