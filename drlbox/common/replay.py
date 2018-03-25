ffd1e801fcd73f8c8c731a537ffb77e0e41c9d4d
spring01 <lihc2012@gmail.com>
2018-03-25 00:05:10 -0400

    two evict modes in priority replay

--- drlbox/common/replay.py
+++ drlbox/common/replay.py
@@ -59,33 +59,46 @@ class PriorityReplay(Replay):
 
     error_eps = 1e-2
 
-    def __init__(self, maxlen, minlen=None,
+    def __init__(self, maxlen, minlen=None, evict_mode='oldest',
                  alpha=0.6, beta=0.4, beta_delta=1e-8):
         super().__init__(maxlen, minlen)
-        self.sum_tree = [0.0] * (2 * self.maxlen - 1)
-        self.sum_tree_kickout = [0.0] * (2 * self.maxlen - 1)
+        size_sum_tree = 2 * self.maxlen - 1
+        self.sum_tree = [0.0] * size_sum_tree
+        self.evict_mode = evict_mode
+        if evict_mode == 'lowest':
+            self.sum_tree_evict = [0.0] * size_sum_tree
         self.alpha = alpha
         self.beta = beta
         self.beta_delta = beta_delta
         self.max_priority = 1.0         # max priority we've ever seen so far
 
     def append(self, transition):
-        self.update_sumtree(self.index, self.max_priority)
-        super().append(transition)
+        if len(self) < self.maxlen or self.evict_mode == 'oldest':
+            self.update_idx(self.index, self.max_priority)
+            super().append(transition)
+        else:
+            _, evict_idx, _ = self.sample_sum_tree(1, self.sum_tree_evict)
+            evict_idx = evict_idx[0]
+            self.update_idx(evict_idx, self.max_priority)
+            self.ring_buffer[evict_idx] = transition
 
     def sample(self, batch_size):
-        len_seg = self.sum_tree[0] / batch_size
+        batch_result = self.sample_sum_tree(batch_size, self.sum_tree)
+        self.beta = min(1.0, self.beta + self.beta_delta)
+        return batch_result
+
+    def sample_sum_tree(self, batch_size, sum_tree):
+        len_seg = sum_tree[0] / batch_size
         rand_list = [(random.random() + i) * len_seg for i in range(batch_size)]
         batch, batch_idx, batch_weights = [], [], []
         for rand in rand_list:
-            ring_idx, priority, transition = self.get_leaf(rand)
+            ring_idx, priority, transition = self.get_leaf(rand, sum_tree)
             batch.append(transition)
             batch_idx.append(ring_idx)
-            weight = (priority / self.sum_tree[0])**(-self.beta)
+            weight = (priority / sum_tree[0])**(-self.beta)
             batch_weights.append(weight)
         max_weight = max(batch_weights)
         batch_weights = [w / max_weight for w in batch_weights]
-        self.beta = min(1.0, self.beta + self.beta_delta)
         return batch, batch_idx, batch_weights
 
     def update_priority(self, batch_idx, batch_error):
@@ -93,9 +106,9 @@ class PriorityReplay(Replay):
             priority = abs(error) + self.error_eps
             priority **= self.alpha
             self.max_priority = max(self.max_priority, priority)
-            self.update_sumtree(ring_idx, priority)
+            self.update_idx(ring_idx, priority)
 
-    def get_leaf(self, value):
+    def get_leaf(self, value, sum_tree):
         """
         Tree structure and array storage:
         Tree index:
@@ -111,24 +124,29 @@ class PriorityReplay(Replay):
         while True:
             left = 2 * parent + 1
             right = left + 1
-            if left >= len(self.sum_tree):  # reach bottom, end search
+            if left >= len(sum_tree):  # reach bottom, end search
                 leaf_idx = parent
                 break
             else:
-                if value <= self.sum_tree[left]:
+                if value <= sum_tree[left]:
                     parent = left
                 else:
-                    value -= self.sum_tree[left]
+                    value -= sum_tree[left]
                     parent = right
         ring_idx = leaf_idx - (self.maxlen - 1)
-        return ring_idx, self.sum_tree[leaf_idx], self.ring_buffer[ring_idx]
+        return ring_idx, sum_tree[leaf_idx], self.ring_buffer[ring_idx]
 
-    def update_sumtree(self, ring_idx, priority):
+    def update_idx(self, ring_idx, priority):
+        self.update_sum_tree(ring_idx, priority, self.sum_tree)
+        if self.evict_mode == 'lowest':
+            self.update_sum_tree(ring_idx, 1.0 / priority, self.sum_tree_evict)
+
+    def update_sum_tree(self, ring_idx, priority, sum_tree):
         leaf_idx = ring_idx + (self.maxlen - 1)
-        change = priority - self.sum_tree[leaf_idx]
-        self.sum_tree[leaf_idx] = priority
+        change = priority - sum_tree[leaf_idx]
+        sum_tree[leaf_idx] = priority
         while leaf_idx:
             leaf_idx = (leaf_idx - 1) // 2
-            self.sum_tree[leaf_idx] += change
+            sum_tree[leaf_idx] += change
 
 
