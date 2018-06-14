07d409f838c97246ae1eb4ea59fcec7fc2eb25b4
spring01 <lihc2012@gmail.com>
2018-06-14 15:16:43 -0400

    layer collection type checking

--- drlbox/net/kfac/build_layer_collection.py
+++ drlbox/net/kfac/build_layer_collection.py
@@ -1,4 +1,4 @@
-
+"""Build a layer collection object as needed by the K-FAC optimizer."""
 from tensorflow import keras
 from drlbox.layer.noisy_dense import NoisyDenseIG, NoisyDenseFG
 from .layer_collection import LayerCollectionExt
@@ -7,6 +7,14 @@ from .layer_collection import LayerCollectionExt
 NOISY_NOT_REG = 'layer_collection register is not implemented for NoisyDenseIG'
 
 def build_layer_collection(layer_list, loss_list):
+    """Returns a layer collection object
+    Args:
+        layer_list: a list of Keras layers (usually model.layers)
+        loss_list: a list of tuples, each tuple is of form (loss_type, args),
+            where loss_type is a string with choices 'normal_predictive' or
+            'categorical_predictive', and args is the input arguments to
+            the "loss-registration" functions in the LayerCollection class.
+    """
     lc = LayerCollectionExt()
 
     # register layers
@@ -44,5 +52,7 @@ def build_layer_collection(layer_list, loss_list):
             lc.register_normal_predictive_distribution(*args)
         elif loss_type == 'categorical_predictive':
             lc.register_categorical_predictive_distribution(*args)
+        else:
+            raise ValueError('loss type {} not supported'.format(loss_type))
 
     return lc
